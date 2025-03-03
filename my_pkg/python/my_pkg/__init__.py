3c9ffa4d98cdbc8a32b050366d8616eca44e0fcd
Cristian Le <git@lecris.dev>
2025-03-03 14:04:24 +0100

    Try to manually add everything to `os.add_dll_directory`

--- my_pkg/python/my_pkg/__init__.py
+++ my_pkg/python/my_pkg/__init__.py
@@ -1,4 +1,9 @@
 import os
+import platform
 
 curr_path = os.getenv("PATH")
 print(curr_path)
+
+if platform.system() == "Windows":
+    for path in curr_path.split(";"):
+        os.add_dll_directory(path)
