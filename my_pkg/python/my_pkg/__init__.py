d1d3e823172825627b6bb3a59dc2707bb165fa98
Cristian Le <git@lecris.dev>
2025-03-03 14:13:55 +0100

    Add scripts dir explicitly

--- my_pkg/python/my_pkg/__init__.py
+++ my_pkg/python/my_pkg/__init__.py
@@ -1,13 +1,7 @@
-import os
 import platform
 
-curr_path = os.getenv("PATH")
-print(curr_path)
-
 if platform.system() == "Windows":
-    from pathlib import Path
+    import sysconfig
+    import os
 
-    for path in curr_path.split(";"):
-        if not Path(path).exists():
-            continue
-        os.add_dll_directory(path)
+    os.add_dll_directory(sysconfig.get_path("scripts"))
