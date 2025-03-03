9ebf98d6b7ec19a4f73a2680ddf4a4d7beafb07c
Cristian Le <git@lecris.dev>
2025-03-03 14:08:19 +0100

    Skip some PATH

--- my_pkg/python/my_pkg/__init__.py
+++ my_pkg/python/my_pkg/__init__.py
@@ -5,5 +5,9 @@ curr_path = os.getenv("PATH")
 print(curr_path)
 
 if platform.system() == "Windows":
+    from pathlib import Path
+
     for path in curr_path.split(";"):
+        if not Path(path).exists():
+            continue
         os.add_dll_directory(path)
